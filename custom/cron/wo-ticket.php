24fc24de7bdb48d4c4d5bbe782facb8104c76e47
AnasAliAbbasi <anasaliabbasi32@gmail.com>
2024-10-25 11:47:00 -0700

    initial commit

---
+++ custom/cron/wo-ticket.php
@@ -0,0 +1,102 @@
+<?php
+
+include_once '../includes/functions.php';
+$settings['topicId'] = array(
+    20 => ' WO Form', /* WO Form */
+    21 => 'Technical Review', /* Technical Review  */
+    22 => 'Material Review', /* Material Review */
+    23 => 'Test Flag', /* Test Flag */
+    24 => 'CS Planning', /* CS Planning */
+    25 => 'BOM Load', /* BOM Load */
+);
+
+$woticketcondition = array(
+    'Turnkey' => array( /* SaleType */
+        'No' => array( /* RepeatOrderFlag */
+            'No' => array( /* Revision */
+                20,
+                21,
+                22,
+                25
+            ),
+            'Yes' => array( /* Revision */
+                20,
+                21,
+                22
+            )
+        ),
+        'Yes' => array( /* RepeatOrderFlag */
+            'No' => array( /* Revision */
+                20,
+                21,
+                22
+            ),
+        )
+    ),
+    'Consignmnt' => array( /* SaleType */
+        'No' => array( /* RepeatOrderFlag */
+            'No' => array( /* Revision */
+                20,
+                21
+            ),
+            'Yes' => array( /* Revision */
+                20,
+                21
+            )
+        ),
+        'Yes' => array( /* RepeatOrderFlag */
+            'No' => array( /* Revision */
+                20,
+                21
+            ),
+        )
+    )
+);
+
+processWOTickets($settings, $woticketcondition);
+
+function processWOTickets($settings, $woticketcondition)
+{
+    list($subject, $msg, $customdata) = setCustomData();
+    if (isValidArray($customdata)) {
+        foreach ($customdata as $cs) {
+            $revision = 'No';
+            $topicIds = $woticketcondition[$cs['_wo_saletype']][$cs['_repeat_flag']][$revision];
+            foreach ($topicIds as $topicId) {
+                $topicTitle = $settings['topicId'][$topicId];
+                $ticketId = createTicket($subject . $cs['won'] . ' (' . $topicTitle . ')', $msg, $topicId, $cs);
+                /* Insert In Log Table For Reference */
+                if ($ticketId) {
+                    generateWOLog($ticketId, $topicId, $cs);
+                }
+            }
+        }
+    }
+}
+
+
+
+function setCustomData()
+{
+    $subject = 'Automated WO: ';
+    $msg = '';
+    $arr = getDataFromDB();
+    return array($subject, $msg, $arr);
+}
+
+
+function getDataFromDB()
+{
+    $fields = '_wo.WONumber  as won, _wo.UNIQ_KEY as uniq_key, _wo.SaleType as _wo_saletype, _wo.WOStatus as _wo_status, if(_wo.RepeatOrderFlag = \'Repeat\', "Yes", "No") as _repeat_flag, _wo.WorkOrderDate as _wo_create_date, _wo.StartDate as _wo_start_date, _wo.DueDate as _wo_due_date, _wo.ScheduledCompleteDate as _scheduled_complete_date, _wo.PlannedCompleteDate as _wo_complete_planned_date, _wo.ReleaseDate as _release_date, _wo.CompleteDate as _wo_complete_date, _wo.WOQty as _wo_quantity, _wo.WOCompleteQty as _wo_complete_quantity, _wo.WORemainingQty as _wo_balanace_quantity, _wo.UpdatedUTC as _utc_time, _wo.Customer as _cus_name, _wo.CustomerPONumber as _cus_po, _mi.ItemPartNo as _cus_pn, _mi.ItemRevision as _cus_pn_rev';
+    $query = sprintf('select %1$s from manex_work_orders as _wo INNER JOIN manex_items as _mi on _wo.UNIQ_KEY = _mi.UNIQ_KEY ORDER BY WONumber DESC limit 2', $fields);
+    $result = executeQuery($query);
+    return getDataFromResultSet($result);
+}
+
+
+function generateWOLog($ticketId, $topicId, $data)
+{
+    $query = sprintf('INSERT INTO `_wo_cron_logs` values (NULL, %1$d, %2$d, %3$s, \'%4$s\', \'%5$s\', \'%6$s\', \'%7$s\', UTC_TIMESTAMP())', $ticketId, $topicId, $data['won'], $data['uniq_key'], $data['_cus_pn'], $data['_cus_pn_rev'], json_encode($data));
+    /* echo $query; */
+    $result = executeQuery($query);
+}
---
+++ custom/includes/functions.php
@@ -0,0 +1,161 @@
+<?php
+include_once dirname(__FILE__) . '/settings.php';
+
+function createTicket($subject, $message = null, $topicID = null, $additional_data = null)
+{
+    global $settings;
+
+    $topicId = $topicID ? $topicID : $settings['topicId'];
+    $reporterEmail = $settings['reporterEmail'];
+    $reporterName = $settings['reporterName'];
+    $reporterIP = gethostbyname(gethostname());
+
+    if (empty($subject)) {
+        echo ("No Subject provided. Not creating ticket.\n");
+        return false;
+    };
+
+    if (empty($message)) {
+        $message = $subject;
+    }
+
+    $subject = $settings['subjectPrefix'] . $subject . $settings['subjectSuffix'];
+
+    $data = array(
+        'name'      =>      $reporterName,
+        'email'     =>      $reporterEmail,
+        'phone'   =>      '',
+        'subject'   =>      $subject,
+        'message'   =>      $message,
+        'ip'        =>      $reporterIP,
+        'topicId'   =>      $topicId
+    );
+
+    if (isValidArray($additional_data)) {
+        $data = array_merge($data, $additional_data);
+    }
+
+    function_exists('curl_version') or die('CURL support required');
+    function_exists('json_encode') or die('JSON support required');
+
+    set_time_limit(30);
+
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_URL, $settings['apiURL']);
+    curl_setopt($ch, CURLOPT_POST, 1);
+    curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($data));
+    curl_setopt($ch, CURLOPT_USERAGENT, 'osTicket API Client v1.8');
+    curl_setopt($ch, CURLOPT_HEADER, FALSE);
+    curl_setopt($ch, CURLOPT_HTTPHEADER, array('Expect:', 'X-API-Key: ' . $settings['apiKey']));
+    curl_setopt($ch, CURLOPT_FOLLOWLOCATION, FALSE);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
+    $result = curl_exec($ch);
+    $code = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+    curl_close($ch);
+
+    if ($code != 201) {
+        echo "Unable to create ticket with subject $subject: " . $result . "\n";
+        return false;
+    }
+
+    $ticketId = (int)$result;
+
+    echo "Ticket '$subject' created with id $ticketId\n";
+
+    return $ticketId;
+}
+
+function sanitizeString($string)
+{
+    return decode(br2nl($string));
+}
+
+function br2nl($string)
+{
+    return preg_replace('/\<br(\s*)?\/?\>/i', PHP_EOL, $string);
+}
+
+function decode($s)
+{
+    if (empty($s)) {
+        return $s;
+    }
+
+    $s = str_ireplace("&nbsp;", " ", $s);
+    return trim($s);
+}
+
+function axeDump($val)
+{
+    echo '<pre>';
+    var_dump($val);
+    echo '</pre>';
+}
+
+function isValidArray($arr)
+{
+    $res = false;
+    if (is_array($arr)) {
+        if (sizeof($arr)) {
+            $res = true;
+        }
+    }
+    return $res;
+}
+
+function getFormVals($key, $type)
+{
+    $val = '';
+    if ($type == 'get') {
+        $val = getGetVals($key);
+    } else if ($type == 'post') {
+        $val = getPostVals($key);
+    }
+
+    return $val;
+}
+
+function getGetVals($key)
+{
+    return isset($_GET[$key]) ? trim($_GET[$key]) : '';
+}
+
+function getPostVals($key)
+{
+    return isset($_POST[$key]) ? trim($_POST[$key]) : '';
+}
+
+function executeQuery($query)
+{
+    $link = getConnection();
+    $sql_result = mysqli_query($link, $query);
+    return $sql_result;
+}
+
+function getDataFromResultSet($sql_result)
+{
+    $rowsFound = false;
+    $results = array();
+    while ($row = mysqli_fetch_assoc($sql_result)) {
+        $rowsFound = true;
+        $results[] = $row;
+    }
+    $results = $rowsFound === false ? null : $results;
+    return $results;
+}
+
+function getConnection()
+{
+    global $settings;
+    $link = mysqli_connect($settings['dbHost'], $settings['dbUser'], $settings['dbPass'],  $settings['database']);
+    if (!$link) {
+        die('Could not connect.');
+    } else {
+        return $link;
+    }
+}
+
+function closeConnection($link)
+{
+    mysqli_close($link);
+}
