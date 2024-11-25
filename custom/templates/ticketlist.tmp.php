9c4e22fc77e2f655ecce5237aade5d04325e5e23
AnasAliAbbasi <anasaliabbasi32@gmail.com>
2024-11-25 08:36:27 -0800

    wo ticket tab completed

--- custom/templates/ticketlist.tmp.php
+++ custom/templates/ticketlist.tmp.php
@@ -1,19 +1,44 @@
 <link rel="stylesheet" href="../custom/css/attachments.css?ver=<?php echo AXEVERSION; ?>" />
 <div class="axeaticketlist">
+    
     <?php
+
+    $settings['topicId'] = array(
+        15 => 'MPI Ticket', /* MPI Ticket */
+        20 => ' WO Form', /* WO Form */
+        21 => 'Technical Review', /* Technical Review  */
+        22 => 'Material Review Consigned', /* Material Review Consigned*/
+        26 => 'Material Review Turnkey', /* Material Review Turnkey*/
+        23 => 'Test Flag', /* Test Flag */
+        24 => 'CS Planning', /* CS Planning */
+        25 => 'BOM Load', /* BOM Load */
+    );
+
     if (isValidArray($ticket_list)) {
     ?>
+    <div class="thread-header">
+    </div>
         <div class="thread-body">
+            
+            <h3 style="padding-left: 105px;
+                margin-top: -10px;
+                color: gray;
+                font-family: sans-serif;
+                text-align: left;
+                text-decoration: underline;">Related Ticket List</h3>
             <div class="ticketlist">
                 <?php
+                $a = 1;
                 foreach ($ticket_list as $ticket_item) {
-                    $attach_link = $base . 'ku';
+                    $a++;
+                    $attach_link = $base . '?id='.$ticket_item['ticket_no'];
+                    $topic_name = $settings['topicId'][$ticket_item['topic_id']];
                 ?>
                     <span class="ticketlist-info">
-                        <a class="no-pjax truncate filename" 
+                    <a class="no-pjax truncate" style="text-decoration: none;" 
                             href="<?php echo $attach_link; ?>" 
                             target="_blank">
-                            <?php echo "[ Auto ". $ticket_item['ticket_id']. " - " .$ticket_item['topic_id']  . "-". $ticket_item['wo_number'] . " " . $ticket_item['part_no'] . " " . $ticket_item['revision'] . " ]" ?>
+                            <?php echo "[ Auto ".  $topic_name  . "-". $ticket_item['wo_number'] . " " . $ticket_item['part_no'] . " " . $ticket_item['revision'] . " ]" ?>
                         </a>
                     </span>
                 <?php
@@ -25,7 +50,7 @@
         <?php } else {
         $reload = md5('noattachmentfound' . time());
     ?>
-        <div class="pull-left">This work order not have any tickets</div>
+        <div class="pull-left">This work order no more related ticket</div>
 
         <div class="clear"></div>
     <?php
--- custom/ticketlist.php
+++ custom/ticketlist.php
@@ -8,7 +8,7 @@ function renderTicketsSection($thread_id)
 {
     $wo_arr = getWorKOrderNo($thread_id);
     if(!empty($wo_arr)){
-        $ticket_list = getRelatedTickets($wo_arr[0]['wo_number']);
+        $ticket_list = getRelatedTickets($wo_arr[0]['wo_number'] , $thread_id);
         include_once 'templates/ticketlist.tmp.php';
     }else{
         echo "No Work Order Found";
@@ -22,19 +22,23 @@ function getWorKOrderNo ($thread_id) {
     $thread_id = intval($thread_id);
 
     if ($thread_id) {
-        $query = sprintf('SELECT _wcl.wo_number from sem_ticket _st JOIN _wo_cron_logs _wcl 
-            where _st.number = _wcl.ticket_id
-            and _st.ticket_id = %1$d limit 1', $thread_id);
+        $query = sprintf('SELECT _wcl.wo_number FROM sem_ticket _st JOIN _wo_cron_logs _wcl 
+            ON _st.number = CONCAT("00", _wcl.ticket_id)
+            WHERE _st.ticket_id = %1$d LIMIT 1', $thread_id);
         $result = executeQuery($query);
         return getDataFromResultSet($result);
     }
 }
 
-function getRelatedTickets($wo_number)
+function getRelatedTickets($wo_number , $thread_id )
 {
     $wo_number = intval($wo_number);
     if ($wo_number) {
-        $query = sprintf('SELECT * from _wo_cron_logs _wcl where _wcl.wo_number = %1$d ', $wo_number);
+        $query = sprintf('SELECT _wcl.*, _st.ticket_id AS "ticket_no" 
+            FROM _wo_cron_logs _wcl 
+            JOIN sem_ticket _st
+                ON CONCAT("00", _wcl.ticket_id) = _st.number
+            WHERE _wcl.wo_number = %1$d and _st.ticket_id != %2$d', $wo_number , $thread_id);
         $result = executeQuery($query);
         return getDataFromResultSet($result);
     }
