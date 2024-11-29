2c7fca5df22b1b735f1bfd20f626ad5023d04049
AnasAliAbbasi <anasaliabbasi32@gmail.com>
2024-11-28 21:51:11 -0800

    update ticket close cron to final

--- custom/cron/ticket-close.php
+++ custom/cron/ticket-close.php
@@ -37,7 +37,7 @@ function getDataFromDB($wo_no = '')
 {   
     $fields = 'l.wo_number, l.ticket_id , m.WOStatus, s.status_id , s.number , s.dept_id , s.staff_id , s.topic_id , s.user_id' ;
     $query = sprintf('select %1$s from _wo_cron_logs l join manex_work_orders m on l.wo_number=m.WONumber join sem_ticket s on l.ticket_id=s.ticket_id
-                    where m.WOStatus NOT in ("Closed","Cancel") and s.status_id<3', $fields);
+                    where m.WOStatus in ("Closed","Cancel") and s.status_id<3', $fields);
     
     $result = executeQuery($query);
     return getDataFromResultSet($result);
@@ -60,22 +60,20 @@ function updateEvents($data)
     $query = sprintf(
         'INSERT INTO `sem_thread_event` (`thread_id`, `thread_type`, `event_id`, `staff_id`, `team_id`, `dept_id`, `topic_id`, `data`, `username`, `uid`, `uid_type`, `annulled`, `timestamp`) 
         VALUES (%1$d, \'%2$s\', %3$d, %4$d, %5$d, %6$d, %7$d, \'%8$s\', \'%9$s\', %10$d, \'%11$s\', %12$s, UTC_TIMESTAMP())',
-        $data['ticket_id'],      // %1$d
-        'T',    // %2$s
-        '2',       // %3$d
-        $data['staff_id'],       // %4$d
-        0,        // %5$d
-        $data['dept_id'],        // %6$d
-        $data['topic_id'],       // %7$d
-        "",           // %8$s
-        'Auto',       // %9$s
-        '',            // %10$d
-        'S',       // %11$s
-        0,      // %12$s
+        $data['ticket_id'],      
+        'T',    
+        '2',       
+        $data['staff_id'],      
+        0,        
+        $data['dept_id'],        
+        $data['topic_id'],      
+        "",           
+        'Auto',       
+        '',            
+        'S',       
+        0,      
         date('Y-m-d H:i:s')
     );
-    
-    echo $query;exit;
     /* echo $query; */
     $result = executeQuery($query);
 }
