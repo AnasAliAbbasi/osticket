f947cec294a7b272c757593f3694a54ad6e86928
AnasAliAbbasi <anasaliabbasi32@gmail.com>
2024-12-11 06:51:12 -0800

    updated file of track states

--- custom/cron/track-states.php
+++ custom/cron/track-states.php
@@ -36,9 +36,29 @@ function getStates()
 
         if(!empty($response['work_order_docs'])){
             $monetringChunk['work_order_document'] = $response['work_order_docs'][0]['Document_Folder'];
+            $monetringChunk['work_order_document_date'] = $response['work_order_docs'][0]['UpdatedUTC'];
+            
         }else{
             $monetringChunk['work_order_document'] = '';
+            $monetringChunk['work_order_document_date'] = '';
+
         }
+
+        if(!empty($response['work_order_form_data'])){
+            $monetringChunk['work_order_form_data_count'] = $response['work_order_form_data'][0]['form_data_count'];
+        }else{
+            $monetringChunk['work_order_form_data_count'] = 0;
+        }
+
+        if(!empty($response['work_order_ticket_without_cron'])){
+            $monetringChunk['work_order_all_ticket'] = $response['work_order_ticket_without_cron'][0]['TotalTickets'];
+            $monetringChunk['work_order_ticket_with_cron'] = $response['work_order_ticket_without_cron'][0]['TicketsWithCron'];
+            $monetringChunk['work_order_ticket_without_cron'] = $response['work_order_ticket_without_cron'][0]['TicketsWithoutCron'];
+            
+        }else{
+            $monetringChunk['work_order_form_data_count'] = 0;
+        }
+
         array_push($moneteringData , $monetringChunk);
     }
     
@@ -100,9 +120,29 @@ function getMoniteringData($orderNo)
     $work_order = executeQuery($work_order);
     $dataSequence['work_order'] = getDataFromResultSet($work_order);
 
-    $work_order_docs = sprintf('select a.Document_Folder from manex_work_order_documents a where a.WONumber = "%1$s" ',  $orderNo);
+    $work_order_docs = sprintf('select a.Document_Folder , a.UpdatedUTC from manex_work_order_documents a where a.WONumber = "%1$s" ',  $orderNo);
     $work_order_docs = executeQuery($work_order_docs);
     $dataSequence['work_order_docs'] = getDataFromResultSet($work_order_docs);
+
+    $work_order_form_data = sprintf('select count(*) as form_data_count from sem_form_entry_values a where a.value = "%1$s" ',  $orderNo);
+    $work_order_form_data = executeQuery($work_order_form_data);
+    $dataSequence['work_order_form_data'] = getDataFromResultSet($work_order_form_data);
+
+    $work_order_ticket_without_cron = sprintf('SELECT 
+            COUNT(DISTINCT c.number) AS TotalTickets, 
+            SUM(CASE WHEN b.cron_date IS NOT NULL THEN 1 ELSE 0 END) AS TicketsWithCron,
+            SUM(CASE WHEN b.cron_date IS NULL THEN 1 ELSE 0 END) AS TicketsWithoutCron
+            FROM 
+                manex_work_orders a
+            LEFT JOIN 
+                _wo_cron_logs b ON a.WONumber = b.wo_number
+            INNER JOIN 
+                sem_ticket c ON c.number = CONCAT("00", b.ticket_id)
+            WHERE 
+                a.WONumber = "%1$s" ',  $orderNo);
+                
+    $work_order_ticket_without_cron = executeQuery($work_order_ticket_without_cron);
+    $dataSequence['work_order_ticket_without_cron'] = getDataFromResultSet($work_order_ticket_without_cron);
 
     return $dataSequence;
 
